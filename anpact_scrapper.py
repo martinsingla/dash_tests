3fa0d8f5ec07b714f004ca95e7647d7a33910131
martinsingla <martinsingla43@gmail.com>
2022-05-05 01:08:49 -0500

    make it better

--- anpact_scrapper.py
+++ anpact_scrapper.py
@@ -29,8 +29,10 @@ from datetime import date
 def scrap_last_ANPACT_records():
     
     #Set connection to Dropbox
-    DBX_APP_KEY= os.environ.get("DBX_APP_KEY")
-    DBX_REFRESH_TOKEN= os.environ.get("DBX_REFRESH_TOKEN")
+    #DBX_APP_KEY= os.environ.get("DBX_APP_KEY")
+    #DBX_REFRESH_TOKEN= os.environ.get("DBX_REFRESH_TOKEN")
+    DBX_APP_KEY= 'thlvigjs1330ab8'
+    DBX_REFRESH_TOKEN= "D5In0ymEO1YAAAAAAAAAAehe9tllSABeIhQ6ram1f_bFjPudIirKNCFcSADFQzor"
     
     dbx = dropbox.Dropbox(oauth2_refresh_token= DBX_REFRESH_TOKEN, app_key=DBX_APP_KEY)
     
